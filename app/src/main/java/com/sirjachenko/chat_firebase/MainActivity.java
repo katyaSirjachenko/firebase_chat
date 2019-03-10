2823bc478024a0aaa3918d3c0c5cca81e44ca964
katya_sirjachenko <sirjachenko.k@gmail.com>
2019-03-10 20:14:54 +0200

    init commit

---
+++ app/src/main/java/com/sirjachenko/chat_firebase/MainActivity.java
@@ -0,0 +1,120 @@
+package com.sirjachenko.chat_firebase;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getName();
+
+    private EditText metText;
+    private Button mbtSent;
+    private DatabaseReference mFirebaseRef;
+
+    private List<Chat> mChats;
+    private RecyclerView mRecyclerView;
+    private ChatAdapter mAdapter;
+    private String mId;
+    private TextView dateText;
+
+    @SuppressLint("HardwareIds")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        metText = (EditText) findViewById(R.id.etText);
+        dateText = findViewById(R.id.dateTextView);
+        mbtSent = (Button) findViewById(R.id.btSent);
+        mRecyclerView = (RecyclerView) findViewById(R.id.rvChat);
+        mChats = new ArrayList<>();
+
+        mId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //mRecyclerView.setItemAnimator(new SlideInOutLeftItemAnimator(mRecyclerView));
+        mAdapter = new ChatAdapter(mChats, mId);
+        mRecyclerView.setAdapter(mAdapter);
+
+        dateText.setText(Calendar.getInstance().getTime().toString()+"\n Created by Katia Siriachenko");
+
+        /**
+         * Firebase - Inicialize
+         */
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        mFirebaseRef = database.getReference("message");
+
+
+        mbtSent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = metText.getText().toString();
+
+                if (!message.isEmpty()) {
+                    /**
+                     * Firebase - Send message
+                     */
+                    mFirebaseRef.push().setValue(new Chat(message, mId));
+                }
+
+                metText.setText("");
+            }
+        });
+
+        mFirebaseRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if (dataSnapshot != null && dataSnapshot.getValue() != null) {
+                    try {
+
+                        Chat model = dataSnapshot.getValue(Chat.class);
+
+                        mChats.add(model);
+                        mRecyclerView.scrollToPosition(mChats.size() - 1);
+                        mAdapter.notifyItemInserted(mChats.size() - 1);
+                    } catch (Exception ex) {
+                        Log.e(TAG, ex.getMessage());
+                    }
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(TAG, databaseError.getMessage());
+            }
+        });
+    }
+}
