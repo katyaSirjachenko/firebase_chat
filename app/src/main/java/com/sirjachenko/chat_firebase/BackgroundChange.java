214b34a812bb3239db2a1c8420756793213757c6
katya_sirjachenko <sirjachenko.k@gmail.com>
2019-03-12 20:32:39 +0200

    backGr

--- app/src/main/java/com/sirjachenko/chat_firebase/BackgroundChange.java
+++ app/src/main/java/com/sirjachenko/chat_firebase/BackgroundChange.java
@@ -15,17 +15,15 @@ public class BackgroundChange {
     public void setNext() {
         switch (backg){
             case R.drawable.chat_background:
-                recyclerView.setBackgroundResource(R.drawable.chat_background1);
                 backg=R.drawable.chat_background1;
                 break;
             case R.drawable.chat_background1:
-                recyclerView.setBackgroundResource(R.drawable.chat_background2);
                 backg=R.drawable.chat_background2;
                 break;
             case R.drawable.chat_background2:
-                recyclerView.setBackgroundResource(R.drawable.chat_background);
                 backg=R.drawable.chat_background;
                 break;
         }
+        recyclerView.setBackgroundResource(backg);
     }
 }
